b185f857ad06bb9b42386a2e98aaee4ba8ae7f70
Tanner Songkakul <tpsongka@ncsu.edu>
2020-08-05 16:12:31 -0400

    Basic plotting works, Bleak seems to have trouble with more than 3 channesl

--- CustomPeripheral.py
+++ CustomPeripheral.py
@@ -1,6 +1,11 @@
-import struct
-from bleak import BleakClient
-from bleak import discover
+from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+from collections import deque
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+from random import randint
+
 
 
 class CustomPeripheral:
@@ -18,6 +23,7 @@ class CustomPeripheral:
         self.CHAR3_DATA =[]
         self.CHAR4_DATA =[]
         self.CHAR5_DATA =[]
+        self.datacount = 0
 
 
     def get_address(self,device_list):
@@ -29,14 +35,49 @@ class CustomPeripheral:
 
     def parse_data(self, sender,data):
         if sender == self.CHAR1:
-            self.CHAR1_DATA.append(data)
+            self.CHAR1_DATA.append(int(data[0]))
+            return 1
         if sender == self.CHAR2:
-            self.CHAR2_DATA.append(data)
+            self.CHAR2_DATA.append(int(data[0]))
+            return 2
         if sender == self.CHAR3:
-            self.CHAR3_DATA.append(data)
+            self.CHAR3_DATA.append(int(data[0]))
+            return 3
         if sender == self.CHAR4:
-            self.CHAR4_DATA.append(data)
+            self.CHAR4_DATA.append(int(data[0]))
+            return 4
         if sender == self.CHAR5:
-            self.CHAR5_DATA.append(data)
+            self.CHAR5_DATA.append(int(data[0]))
+            return 5
+
+
+class CPPlot:
+    def __init__(self,app,win,win_size):
+        self.win_size = win_size
+        # self.p1_data = deque([0]*self.win_size)
+        # self.p2_data = deque([0]*self.win_size)
+        # self.p3_data = deque([0]*self.win_size)
+        # self.p4_data = deque([0]*self.win_size)
+        # self.p5_data = deque([0]*self.win_size)
+        self.plot_data = [deque([0]*self.win_size) for i in range(5)]
+
+        self.app = app
+        self.win = win
+        self.char1_plot = win.addPlot(row=1, col=1, colspan=5, title="Char 1")
+        self.char2_plot = win.addPlot(row=2, col=1, colspan=5, title="Char 2")
+        self.char3_plot = win.addPlot(row=3, col=1, colspan=5, title="Char 3")
+        self.char4_plot = win.addPlot(row=4, col=1, colspan=5, title="Char 4")
+        self.char5_plot = win.addPlot(row=5, col=1, colspan=5, title="Char 5")
+        self.char_plots = [self.char1_plot,self.char2_plot, self.char3_plot,self.char4_plot,self.char5_plot]
+
+    def plot_char(self, char_num, data):
+        char_ind = char_num - 1
+        self.plot_data[char_ind].append(data)
+        self.plot_data[char_ind].popleft()
+        self.char_plots[char_ind].plot(self.plot_data[char_ind], clear= True)
+
+    def update(self):
+        pg.QtGui.QApplication.processEvents()
+
 
 
--- CustomPeripheralInterface.py
+++ CustomPeripheralInterface.py
@@ -1,38 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Notifications
--------------
-Example showing how to add notifications to a characteristic and handle the responses.
-Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
+
 """
 
 import asyncio
 from bleak import BleakClient
 from bleak import discover
 import CustomPeripheral as CPLib
+from pyqtgraph.Qt import QtGui
+import pyqtgraph as pg
 
+# User Settings
 name = "CP001"  # <--- Change to your device's shortened Advertising Name here
-CP = CPLib.CustomPeripheral(name)
+window_length = 100  # Length of rolling windows.
 
-# initialize plot here
-# initialize variables here
-i = 0
+# Globals
+CP = CPLib.CustomPeripheral(name)
+app = QtGui.QApplication([])
+win = pg.GraphicsLayoutWidget(show=True, title="Custom Peripheral")
+CP_plot = CPLib.CPPlot(app, win, window_length)
 
 def notification_handler(sender, data):
     """Simple notification handler which prints the data received."""
     # process data into buffers
-    CP.parse_data(sender, data)
-
-
-
+    CP.datacount = CP.datacount + 1
+    char = CP.parse_data(sender, data)
+    CP_plot.plot_char(char, int(data[0]))
 
 
 async def enable_notif(CP, client):
-    # await client.start_notify(CP.CHAR1, notification_handler)
-    # await client.start_notify(CP.CHAR2, notification_handler)
-    # await client.start_notify(CP.CHAR3, notification_handler)
-    # await client.start_notify(CP.CHAR4, notification_handler)
+    await client.start_notify(CP.CHAR1, notification_handler)
+    await client.start_notify(CP.CHAR2, notification_handler)
+    await client.start_notify(CP.CHAR3, notification_handler)
+    await client.start_notify(CP.CHAR4, notification_handler)
     await client.start_notify(CP.CHAR5, notification_handler)
+    await plot_handler()
+
+
+async def plot_handler():
+    while 1:
+        CP_plot.update()
+        await asyncio.sleep(1)
 
 
 async def disable_notif(CP, client):
@@ -50,25 +58,16 @@ async def run(address, loop, debug=False):
         async with BleakClient(CP.ADDR, loop=loop) as client:
             x = await client.is_connected()
             print("Connected: {0}".format(x))
-
             # await client.write_gatt_char(SYSCFG_UUID, bytes.fromhex(cfg_string))
-
-            # await client.start_notify(HET2.CHAR1, notification_handler)
-            # await client.start_notify(HET2.CHAR2, notification_handler)
-            # await client.start_notify(HET2.CHAR3, notification_handler)
-            # await client.start_notify(HET2.CHAR4, notification_handler)
-            # await client.start_notify(HET2.CHAR5, notification_handler)
-
             await enable_notif(CP, client)
             await asyncio.sleep(1000.0, loop=loop)
-            await disable_notif(CP,client)
+            await disable_notif(CP, client)
     else:
         print("ERROR: No device found.")
 
+
 if __name__ == "__main__":
     import os
-
-    os.environ["PYTHONASYNCIODEBUG"] = str(1)
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(CP, loop, False))
