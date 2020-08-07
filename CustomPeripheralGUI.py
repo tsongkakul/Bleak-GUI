e514e21f7bdd74a39ba197933f0db4c066da4e78
Tanner Songkakul <tpsongka@ncsu.edu>
2020-08-07 14:38:52 -0400

    Switched to update based plotting method.

--- CustomPeripheralGUI.py
+++ CustomPeripheralGUI.py
@@ -1,3 +1,14 @@
+"""
+Tanner Songkakul
+
+CustomPeripheralGUI.py
+
+Uses bleak and PyQT to connect to a CC2642 Launchpad running Custom Peripheral
+and plot data on each characteristics.
+
+Requires bleak, pyqt, qasync and included customperipheral library
+"""
+
 from PyQt5 import QtWidgets
 import sys
 import asyncio
@@ -7,54 +18,61 @@ from bleak import BleakClient
 from bleak import discover
 
 # GLOBALS
+#TODO: find a better way to use this with the notification handler without using as global
 cp = cplib.CustomPeripheral()
 
 def notification_handler(sender, data):
-    # process data into buffers
+    """Handle incoming packets."""
     cp.datacount = cp.datacount + 1
     char = cp.parse_data(sender, data)
 
 
 async def plot_handler(cp, win):
-    while 1:
+    """Asynchronously update the plot each second """
+    while 1: # TODO: remove loop here to allow disconnect/reconnect
         win.plot_all(cp.ALL_DATA)
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
 
 
 async def button_wait(win):
-    while not win.connect_button:  # wait for button press
+    """Polling for button press"""
+    while not win.connect_button:  # wait for button press flag to be set in GUI
         await asyncio.sleep(.01)
-    win.button_ack()
+    win.button_ack() # clear button press flag
 
 
 async def find_device(win, cp):
-    await button_wait(win)
+    """Search for device after button press"""
+    await button_wait(win)  # Wait for button press
     win.display_status("Scanning...")
-    devices = await discover()
-    cp.set_name(win.device_name)
-    device_found = cp.get_address(devices)
+    devices = await discover()  # Discover devices on BLE
+    cp.set_name(win.device_name) # Get device name from text input window in GUI
+    device_found = cp.get_address(devices) #  Check if device in list
     return device_found
 
 
 async def enable_notif(cp, client):
+    """Start notifications on all characteristics"""
     for char in cp.CHAR_LIST:
         await client.start_notify(char, notification_handler)
-    print("Notifications enabled")
+    #print("Notifications enabled")
 
 
 async def disable_notif(cp, client):
+    """Stop notifications on all characteristics"""
     for char in cp.CHAR_LIST:
         await client.stop_notify(char, notification_handler)
 
 
 async def run(win, cp, loop):
-    while 1:
+    """Main asyncio loop"""
+    while 1: # TODO replace this loop to allow for disconnect/reconnect
         # while not cp.CONNECTED:
-        dev_found = await find_device(win, cp)
+        dev_found = await find_device(win, cp) # Find device address
         if dev_found:
             win.display_status("Device {} found! Connecting...".format(cp.NAME))
             async with BleakClient(cp.ADDR, loop=loop) as client:
-                x = await client.is_connected()
+                x = await client.is_connected() # Attempt device connection TODO add error messages
                 win.display_status("Connected!")
                 await enable_notif(cp, client)
                 await plot_handler(cp, win)
